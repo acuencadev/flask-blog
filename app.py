1f99b496dda74a02cf2d600b360892961df5d3b3
Amador Cuenca <sphi02ac@gmail.com>
2018-09-03 18:03:37 -0500

    feat: Add DB configuration to the blog controller

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+# app.py - controller
+from flask import Flask, render_template, request, sessions, flash, redirect, url_for, g
+
+# imports
+import sqlite3
+
+
+# configuration
+DATABASE = "blog.db"
 
 app = Flask(__name__)
 
+app.config.from_object(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# function used for connecting to the database
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
 
 if __name__ == '__main__':
